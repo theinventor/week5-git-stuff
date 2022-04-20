510305e31f61a00d70e85883ba9f230cb3d0545d
Troy Anderson <the.inventor@gmail.com>
2022-04-20 12:24:45 -0700

    Revert "Comments feature"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts do
-    resources :comments
-  end
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_20_191119) do
-  create_table "comments", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_04_20_184601) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -26,5 +18,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_20_191119) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts"
 end
