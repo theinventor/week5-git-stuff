9e894be237cf08409f801e446ea8ab0f9213eca9
Troy Anderson <the.inventor@gmail.com>
2022-04-20 12:25:21 -0700

    Revert "Revert "Comments feature""

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_20_184601) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_20_191119) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,4 +26,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_20_184601) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
